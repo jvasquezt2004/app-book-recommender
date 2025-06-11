9669b62cd1ab16c2af74a1697c4682ebe5827614
Jorge Alonso <jorgealonsovasquez@hotmail.com>
2025-06-10 23:07:00 -0600

    feat: enhance BookCard with pressable wrapper and improved UI elements

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,8 +1,9 @@
 import { Link } from "expo-router";
-import { View } from "react-native";
+import { Pressable, View } from "react-native";
 import { Card } from "./ui/card";
 import { Heading } from "./ui/heading";
 import { HStack } from "./ui/hstack";
+import { ArrowRightIcon, Icon } from "./ui/icon";
 import { Image } from "./ui/image";
 import { Text } from "./ui/text";
 
@@ -25,20 +26,23 @@ type BookCardProps = {
 
 export function BookCard({ book }: BookCardProps) {
   return (
-    <Card className="w-[48%] mb-4 bg-gray-800 p-2 rounded-xl">
+    <Link href={`/book/${book.id}`} asChild>
+    <Pressable className="w-[48%] mb-4"> 
+    <Card className="bg-gray-800 p-2 rounded-xl min-h-80 justify-between">
       <Image source={{ uri: book.metadata.thumbnail }} className="w-full h-48 rounded-lg mb-6 " alt={book.metadata.title} />
       <View className="flex flex-row justify-between">
-        <Text className="text-white text-sm mb-2 text-typography-700">{book.metadata.authors}</Text>
+        <Text className="text-white text-sm mb-2">{book.metadata.authors}</Text>
         {book.metadata.published_year && (
-          <Text className="text-white text-sm mb-2 text-typography-700">{book.metadata.published_year}</Text>
+          <Text className="text-white text-sm mb-2">{book.metadata.published_year}</Text>
         )}
       </View>
-      <Heading size="md" className="text-white mb-4">{book.metadata.title}</Heading>
-      <HStack>
-        <Link href={`/book/${book.id}`}>
-          <Text className="text-white text-sm mb-2 text-typography-700">Read</Text>
-        </Link>
+      <Heading size="md" className="text-white mb-4" numberOfLines={2} ellipsizeMode="tail">{book.metadata.title}</Heading>
+      <HStack className="items-center">
+        <Text size="sm" className="font-semibold text-info-600 no-underline">Check Book</Text>
+        <Icon as={ArrowRightIcon} size="sm" className="text-info-600 mt-0.5 ml-0.5" />
       </HStack>
     </Card>
+    </Pressable>
+    </Link>
   )
 }
